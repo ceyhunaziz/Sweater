2485a9435897c04e8e1e0535b1d86d90fea99e1f
ceyhunaziz <40896664+ceyhunaziz@users.noreply.github.com>
2019-01-28 17:21:29 +0400

    one to many

--- src/main/java/com/example/sweater/domain/Role.java
+++ src/main/java/com/example/sweater/domain/Role.java
@@ -1,8 +1,15 @@
 package com.example.sweater.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+
 /**
  * Created by User on 27.01.2019.
  */
-public enum Role {
-    USER;
+public enum Role implements GrantedAuthority{
+    USER {
+        @Override
+        public String getAuthority() {
+            return name();
+        }
+    };
 }
