157559ca0b1485a88fa2737195cf1554b7421630
ceyhunaziz <40896664+ceyhunaziz@users.noreply.github.com>
2019-01-31 16:58:43 +0400

    Added new User Role - ADMIN.

--- src/main/java/com/example/sweater/domain/Role.java
+++ src/main/java/com/example/sweater/domain/Role.java
@@ -1,15 +1,11 @@
 package com.example.sweater.domain;
-
 import org.springframework.security.core.GrantedAuthority;
 
-/**
- * Created by User on 27.01.2019.
- */
 public enum Role implements GrantedAuthority{
-    USER {
-        @Override
-        public String getAuthority() {
-            return name();
-        }
-    };
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
